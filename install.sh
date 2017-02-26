00e24972a53bd988a812b86b261b29beddd3f40c
Zachary Paladino <paladizm@gmail.com>
2017-02-25 21:11:47 -0500

    added a real dumb install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+#Install all at once. break up if you want. 
+brew install zsh zsh-completions && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k && git clone https://github.com/powerline/fonts.git ~/powerline-fonts && . ~/powerline-fonts/install.sh
+
